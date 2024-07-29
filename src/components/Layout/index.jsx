a04d507218d3ab9aa40d42aab67362d0c9e25a97
Idriss Cisse <aliouc140@gmail.com>
2024-07-29 02:19:30 +0200

    Mise en page du layout principal

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
+import './index.scss' ;
 
 const Layout = () => (
-  <div>
-    <header>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+  <div className='layout'>
+    <header className='header'>
+      <div className='logo'>
+        <img src="/assets/header-logo.svg" alt="Logo de Kasa" />
+      </div>
+      <nav className='nav'>
+        <NavLink to="/"  
+        className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>Accueil</NavLink>
+        <NavLink to="/about"  
+        className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>A Propos</NavLink>
       </nav>
     </header>
-    <main>
+    <main className='main'>
       <Outlet />
     </main>
-    <footer>
-      <p>Footer content</p>
+    <footer className='footer'>
+    <img src="/assets/footer-logo.svg" alt="Logo de Kasa" />
+    <p>© 2020 Kasa. All rights reserved</p>
     </footer>
   </div>
 );
